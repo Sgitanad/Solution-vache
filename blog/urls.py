96ef7b085bf97f5ed00fab7eda09fa79bd63d71b
sanad <sanadbelhaj59@gmail.com>
2025-01-28 11:56:14 +0100

    update 2

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.main, name='main'),
     path('/next', views.next, name='next'),
+    path('/post_liste', views.post_liste, name='post_liste'),
+    path('post/new/', views.post_new, name='post_new'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
 
 def main(request):
     return render(request, 'blog/main.html', {})
 
 def next(request):
     return render(request, 'blog/next.html', {})
+
+def post_liste(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post-liste.html', {})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('next', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_new.html', {'form': form})
